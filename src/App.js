21520d5510c5eea297d36a550c134a87c2494f2f
mohsin-8 <mohsinalikhan518@gmail.com>
2021-08-27 00:47:14 +0500

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   let data = { title: "Waiting for Data" };
-
   const [todo, setTodo] = useState(data);
+  const [isFetching, setFetching] = useState(data);
 
   useEffect(() => {
-
     async function fetchData() {
+      setFetching(true);
       const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      console.log("response = ", response);
+      console.log('response: ', response);
 
-      let data2 = await response.json()
+      let data2 = await response.json();
       setTodo(data2);
-
-      console.log("Data = ", todo);
+      setFetching(false);
+      console.log('Data: ', todo);
     }
     fetchData();
   }, [6]);
-
+  if (isFetching) {
+    return <div>Data Loading...</div>
+  }
   return (
     <div>
-      Hello world: {todo.title} <br />
-      Id: {todo.id} <br />
-      UserId: {todo.userId}
+      Hello World: {todo.title}
+      <p>{todo.userId}</p>
     </div>
   )
 }
